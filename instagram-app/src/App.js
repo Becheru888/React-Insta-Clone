c340b66116e96ca397576455f8cd8b4d2586e632
Becheru888 <becheru93@gmail.com>
2019-06-03 15:08:34 +0100

    Data collected

---
+++ instagram-app/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer'
+import dummyData from './dummy-data';
+import './App.css';
+
+
+
+
+
+
+console.log(dummyData)
+
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar />
+      <PostContainer data={dummyData}/>
+    </div>
+  );
+}
+
+export default App;
+
+
+// {
+//   this.state.friends.map(friend => (
+//     <Friend
+//       key={friend.id}
+//       friend={friend}
+//       deleteFriend={this.deleteFriend}
+//       markAsEnemy={this.markAsEnemy}
+//       setFriendToBeEdited={this.setFriendToBeEdited}
+//     />
+//   ))
+// }
---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class SearchBar extends React.Component{
+    constructor(props){
+        super(props)
+
+    }
+
+
+    render(){
+        return <h1></h1>
+    }
+}
+
+export default SearchBar
