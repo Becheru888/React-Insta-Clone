443da032329d30325bfc3efea873c44cd469b807
Becheru888 <becheru93@gmail.com>
2019-06-05 13:42:05 +0100

    PostPage updated with the new content

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import SearchBarContainer from './components/SearchBar/SearchBar';
-import PostContainer from './components/PostContainer/PostContainer'
-import dummyData from './dummy-data';
 import './App.css';
-
+import PostPage from './components/PostContainer/PostPage'
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      postData: [],
-    };
-  }
 
-  componentDidMount() {
-    this.setState({ postData: dummyData });
-  }
 
 
   render() {
 
     return (
       <div className="App">
-        <SearchBarContainer />
-        <PostContainer data={this.state.postData} />
+        <PostPage/>
       </div>
     );
   }
