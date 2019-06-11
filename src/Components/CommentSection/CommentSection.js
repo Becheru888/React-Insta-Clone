d944155bc159bca88bd7a48d4ad4944164dc49fa
Becheru888 <becheru93@gmail.com>
2019-06-11 13:06:46 +0100

    Comments component functionality added

--- src/Components/CommentSection/CommentSection.js
+++ src/Components/CommentSection/CommentSection.js
@@ -7,18 +7,53 @@ class CommentSection extends React.Component {
         super(props)
         this.state = {
             comments : this.props.comments,
+            newComment: '',
         }
+
+        
+    this.changeHandler = this.changeHandler.bind(this);
+    this.addNewComment = this.addNewComment.bind(this);
     }
+
+
+changeHandler = e => {
+    this.setState({newComment: e.target.value})
+}
+
+addNewComment = e => {
+    e.preventDefault();
+    this.setState({
+          comments: this.state.comments.concat({
+           key : Math.floor(Math.random() * 99999),
+           username: 'Remi',
+           text:this.state.newComment
+        }),
+        newComment:''
+    })
+}
 
     
     render(){
       
         return (
             <PostWrapper>
-            {this.props.comments.map(comment => <div key={Math.floor(Math.random() * 99999)}><p><span>{comment.username}</span>{comment.text}</p></div>)}
+            {this.state.comments.map(comment => <div key={Math.floor(Math.random() * 99999)}><p><span>{comment.username}</span>{comment.text}</p></div>)}
             <div><Moment  className='stamp' fromNow>{this.props.date}</Moment ></div>
-            <form>
-            <div><input type='text' placeholder='Add comment...' className='comm-input'/></div>
+
+            <form onSubmit={this.addNewComment}>
+            <div>
+
+                <input 
+
+                type='text' 
+                placeholder='Add comment...' 
+                className='comm-input'
+                onChange={this.changeHandler}
+                value={this.state.newComment}
+                />
+                
+                  
+            </div>
             </form>   
             </PostWrapper>
         )
